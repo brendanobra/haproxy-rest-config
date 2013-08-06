f7dbbaa302eb711382d63da48084d844d5db0cfc
Brendan O'Bra <brendan@obrafamily.org>
2013-08-06 12:58:40 -0700

    Adding working Swagger deps

--- project/Build.scala
+++ project/Build.scala
@@ -6,16 +6,26 @@ object ApplicationBuild extends Build {
 
   val appName         = "test"
   val appVersion      = "1.0-SNAPSHOT"
-
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+ "com.wordnik" %% "swagger-play2-utils" % "1.2.5"
+
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    // Add your own project settings here     
+//	resolvers += "Sonatype OSS Snapshots for Wordnik" at "https://oss.sonatype.org/content/repositories/snapshots/",
+//	resolvers += "Maven.org repo1" at "http://repo1.maven.org/maven2/"   
+	
+    resolvers := Seq(
+      "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
+      "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
+      "java-net" at "http://download.java.net/maven/2",
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/")  
+)
 
 }
